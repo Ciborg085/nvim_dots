b3c80ae42668e84cdfee257ae779f1bb9fb298ef
Bernardo Frija <1903bernardo@gmail.com>
2024-09-23 17:27:25 +0100

    FZF and fugitive changes

--- lua/ciborg/plugins/fugitive.lua
+++ lua/ciborg/plugins/fugitive.lua
@@ -26,6 +26,10 @@ return {
                 vim.keymap.set("n","<leader>P", function()
                     vim.cmd.Git({'pull', "--rebase"})
                 end, opts)
+
+                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+                -- needed if i did not set the branch up correctly
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
             end
         })
     end
---
+++ lua/ciborg/plugins/fzf.lua
@@ -0,0 +1,9 @@
+return {
+    "junegunn/fzf.vim",
+    dependencies = {
+        "junegunn/fzf"
+    },
+    config = function()
+        -- vim.remap.set("n","<leader>pf", vim.cmd.FZF)
+    end,
+}
